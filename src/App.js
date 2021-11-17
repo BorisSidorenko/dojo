55f327d4c32225095ed8f496ad0c08590e269ebf
Boris <boris.m.sidorenko@gmail.com>
2021-11-17 21:13:20 +0300

    added Sidebar component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Login from './pages/login/Login';
 import Project from './pages/project/Project';
 import Signup from './pages/signup/Signup';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Sidebar />
         <div className="container">
           <Navbar />
           <Switch>
