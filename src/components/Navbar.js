415351e956496c0310c6af208e7bdbe35b4bdace
Boris <boris.m.sidorenko@gmail.com>
2021-11-18 21:27:32 +0300

    logging the user out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
 import './Navbar.css';
 import Temple from '../assets/temple.svg';
 
 export default function Navbar() {
+    const { logout, error, isPending } = useLogout();
+
     return (
         <div className="navbar">
             <ul>
@@ -17,7 +20,9 @@ export default function Navbar() {
                     <Link to="/signup">Signup</Link>
                 </li>
                 <li>
-                    <button className="btn">Logout</button>
+                    {!isPending && <button className="btn" onClick={logout}>Logout</button>}
+                    {isPending && <button className="btn" disabled>Loging out...</button>}
+                    {error && <button className="error">{error}</button>}
                 </li>
             </ul>
         </div>
--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { projectAuth, projectStorage } from '../firebase/config';
+import { projectAuth, projectStorage, projectFirestore } from '../firebase/config';
 import { useAuthContext } from '../hooks/useAuthContext';
 
 export const useSignup = () => {
@@ -13,18 +13,29 @@ export const useSignup = () => {
         setIsPending(true);
 
         try {
+            // singup
             const res = await projectAuth.createUserWithEmailAndPassword(email, password);            
 
             if (!res) {
                 throw new Error('Could not complete signup');
             }
 
+            // upload user thumbnail
             const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`;
             const img = await projectStorage.ref(uploadPath).put(thumbnail);
             const photoURL = await img.ref.getDownloadURL();
 
+            // add display name to user 
             await res.user.updateProfile({ displayName, photoURL });
 
+            // create a user document
+            await projectFirestore.collection('users').doc(res.user.uid).set({
+                online: true,
+                displayName,
+                photoURL
+            });
+
+            // displatch login action
             dispatch({ type: 'LOGIN', payload: res.user });
 
             if (!isCancelled) {
