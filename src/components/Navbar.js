3f094fd1d5d01b5aaf03fbf749991ffbe827f2bc
Boris <boris.m.sidorenko@gmail.com>
2021-11-20 18:45:47 +0300

    redirect user after login/logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 import './Navbar.css';
 import Temple from '../assets/temple.svg';
 
 export default function Navbar() {
+    const { user } = useAuthContext();
     const { logout, error, isPending } = useLogout();
 
     return (
@@ -13,17 +15,25 @@ export default function Navbar() {
                     <img src={Temple} alt="dojo logo" />
                     <span>The Dojo</span>
                 </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/signup">Signup</Link>
-                </li>
-                <li>
-                    {!isPending && <button className="btn" onClick={logout}>Logout</button>}
-                    {isPending && <button className="btn" disabled>Loging out...</button>}
-                    {error && <button className="error">{error}</button>}
-                </li>
+                {!user && (
+                    <>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/signup">Signup</Link>
+                        </li>
+                    </>
+                )}
+                {user && (
+                    <>
+                        <li>
+                            {!isPending && <button className="btn" onClick={logout}>Logout</button>}
+                            {isPending && <button className="btn" disabled>Loging out...</button>}
+                            {error && <button className="error">{error}</button>}
+                        </li>
+                    </>
+                )}                
             </ul>
         </div>
     )
